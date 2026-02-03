7112831281df92fe62b74aac0d1b0d1880ff05c4
Hibanaw Hu <hibanaw@qq.com>
2026-02-03 19:45:50 +0800

    重要：修复阈值分割的错误

--- include/dv/binaryzation.hpp
+++ include/dv/binaryzation.hpp
@@ -21,10 +21,10 @@ namespace dv
                 {
                     TPFT pixel;
                     pixel_cast(src(x, y), pixel);
-                    if (pixel <= t_low || pixel >= t_high)
-                        dst(x, y) = BinaryPixel{0};
-                    else
+                    if (pixel >= t_low && pixel <= t_high)
                         dst(x, y) = BinaryPixel{255};
+                    else
+                        dst(x, y) = BinaryPixel{0};
                 }
             }
         }
